ba401bdcbce0d194fb39ccac814bc46f8eeb0c55
alexismichia <alexi890@outlook.com>
2023-06-08 16:04:53 -0300

    db con algunas relaciones

--- api/src/db.js
+++ api/src/db.js
@@ -2,6 +2,17 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
+const Team = require('./models/team');
+const Player = require('./models/player');
+const Standings = require('./models/standings');
+const TeamSquad = require('./models/team_squad');
+const Coach = require('./models/coach');
+const Referee = require('./models/referee');
+const Venue = require('./models/venue');
+const TVStation = require('./models/tv_station');
+const Probabilities = require('./models/probabilities');
+const Rivals = require('./models/rivals');
+const News = require('./models/news');
 const {
   DB_USER, DB_PASSWORD, DB_HOST,DATABASE_URL
 } = process.env;
@@ -36,14 +47,44 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const {  } = sequelize.models;
+const {
+  Team,
+  Player,
+  Standings,
+  TeamSquad,
+  Coach,
+  Referee,
+  Venue,
+  TVStation,
+  Probabilities,
+  Rivals,
+  News,  } = sequelize.models;
 
 // Aca vendrian las relaciones
+Team.hasMany(Player, { foreignKey: 'team_id' });
+Player.belongsTo(Team, { foreignKey: 'team_id' });
+
+TeamSquad.belongsTo(Player, { foreignKey: 'player_id' });
+TeamSquad.belongsTo(Team, { foreignKey: 'team_id' });
+
+Team.hasMany(Coach, { foreignKey: 'team_id' });
+Coach.belongsTo(Team, { foreignKey: 'team_id' });
 
 
 // Product.hasMany(Reviews);
 
 module.exports = {
   conn: sequelize,
-  
+  Team,
+  Player,
+  Standings,
+  TeamSquad,
+  Coach,
+  Referee,
+  Venue,
+  TVStation,
+  Probabilities,
+  Rivals,
+  News,
+  // Agrega los demás modelos aquí...
 };
