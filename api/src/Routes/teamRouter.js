ec9a0a43fca7d2802366487b252edc7e2fa7c800
Pedro Bocco <pedrobocco@Pedros-MacBook-Air.local>
2023-06-12 12:03:12 -0300

    routesteam

--- api/src/Routes/teamRouter.js
+++ api/src/Routes/teamRouter.js
@@ -1,12 +1,9 @@
-const express = require("express");
-const { getTeams } = require("../controllers/team/getTeam");
+const express = require('express')
 
-const teamRouter = express.Router();
+const teamRouter = express.Router()
 
 const {getTeam} = require('../controllers/team/getTeam.js')
 
 teamRouter.get('/:id', getTeam)
 
 module.exports = teamRouter
-
-
