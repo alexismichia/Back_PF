1d8c0d6c0bcbe555c53927886e7d2481f270f953
oceanamassano <oshimassano@gmail.com>
2023-06-13 20:48:17 -0300

    getleaguebyteam with paginate

--- api/src/services/league/getLeagueByTeam.service.js
+++ api/src/services/league/getLeagueByTeam.service.js
@@ -4,16 +4,31 @@ require("dotenv").config();
 const API_KEY = process.env.API_KEY;
 const BASE_URL = "https://api.sportmonks.com/v3/football/leagues/teams";
 
-exports.getLeagueTeamFromAPI = async (id) =>{
-    const URL = `${BASE_URL}/${id}`
-    try {
-        const response = await axios.get(URL,{
-            params:{
-              api_token: API_KEY
-            }});
-            return response.data.data
-        }
-     catch (error) {
-        console.error(`Error fetching data from API: ${error}`);
-        throw error;
-    }}
+exports.getLeagueTeamFromAPI = async (id) => {
+  let URL = `${BASE_URL}/${id}`;
+  const allData = []
+  try {
+    while (URL) {
+      const response = await axios.get(URL, {
+        params: {
+            per_page:2,
+          api_token: API_KEY,
+        },
+      });
+      const responseData = response.data.data;
+      allData.push(...responseData)
+
+      const pagination = response.data.pagination
+      URL = pagination.next_page;
+
+      if(!pagination.has_more){
+        URL= null
+      }
+    }
+    return allData
+    
+  } catch (error) {
+    console.error(`Error fetching data from API: ${error}`);
+    throw error;
+  }
+};
