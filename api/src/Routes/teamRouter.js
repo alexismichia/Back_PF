c8346d563fd14f42dc2df291a5494f9ae0a3b9a4
Pedro Bocco <pedrobocco@Pedros-MacBook-Air.local>
2023-06-12 23:12:32 -0300

    getTeamBySeason done

--- api/src/Routes/teamRouter.js
+++ api/src/Routes/teamRouter.js
@@ -3,9 +3,12 @@ const express = require('express')
 const teamRouter = express.Router()
 
 const {getTeam} = require('../controllers/team/getTeamByCountry.js')
-const {getTeamsById} = require('../controllers/team/getTeamById.js')    
+const {getTeamById} = require('../controllers/team/getTeamById.js')    
+const {getTeamBySeason} = require('../controllers/team/getTeamBySeason.js')
+
 
 teamRouter.get('/country/:id', getTeam)
-teamRouter.get('/:id', getTeamsById)
+teamRouter.get('/:id', getTeamById)
+teamRouter.get('/season/:id', getTeamBySeason)
 
 module.exports = teamRouter
