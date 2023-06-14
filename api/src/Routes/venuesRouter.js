aa1145b137fa3e340cd0f6ddd4c373c1fd446e9e
alexismichia <alexi890@outlook.com>
2023-06-13 21:31:24 -0300

    venues?2.0

--- api/src/Routes/venuesRouter.js
+++ api/src/Routes/venuesRouter.js
@@ -1,6 +1,6 @@
 const express = require("express")
-const getVenuesById= require("../controllers/venues/getVenuesById")
-const getVenuesByName= require("../controllers/venues/getVenuesByName")
+const {getVenuesById}= require("../controllers/venues/getVenuesById")
+const {getVenuesByName}= require("../controllers/venues/getVenuesByName")
 
 const venuesRouter = express.Router();
 
