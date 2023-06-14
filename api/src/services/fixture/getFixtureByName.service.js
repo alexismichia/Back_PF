9ec4f3efc1852b5184528c1174082d729b865122
oceanamassano <oshimassano@gmail.com>
2023-06-14 11:19:17 -0300

    with pagination

--- api/src/services/fixture/getFixtureByName.service.js
+++ api/src/services/fixture/getFixtureByName.service.js
@@ -6,13 +6,26 @@ const BASE_URL = "https://api.sportmonks.com/v3/football/fixtures/search";
 
 exports.getFixtureByNameFromAPI = async (name) => {
   const URL = `${BASE_URL}/${name}`;
+  const allData = [];
   try {
-    const response = await axios.get(URL, {
-      params: {
-        api_token: API_KEY,
-      },
-    });
-    return response.data.data;
+    while (URL) {
+      const response = await axios.get(URL, {
+        params: {
+          per_page: 2,
+          api_token: API_KEY,
+        },
+      });
+      const responseData = response.data.data;
+      allData.push(...responseData);
+
+      const pagination = response.data.pagination;
+      URL = pagination.next_page;
+
+      if (!pagination.has_more) {
+        URL = null;
+      }
+    }
+    return allData;
   } catch (error) {
     console.error(`Error fetching data from API: ${error}`);
     throw error;
