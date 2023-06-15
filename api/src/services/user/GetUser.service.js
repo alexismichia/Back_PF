db69086bcbcdaf9b01773b64b3ee453cf64a19ac
oceanamassano <oshimassano@gmail.com>
2023-06-15 14:03:09 -0300

    checking console

--- api/src/services/user/GetUser.service.js
+++ api/src/services/user/GetUser.service.js
@@ -3,6 +3,7 @@ const {User}= require('../../db')
 exports.getAllUsersAsync = async () => {
   try {
     const allUsers = await User.findAll();
+    console.log(allUsers)
     return allUsers;
   } catch (error) {
     console.error(`Error fetching users from the database: ${error}`);
