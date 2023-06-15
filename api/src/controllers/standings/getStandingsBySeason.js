c4cbccd4cc0c652d2e1ffdcc64fcb8525cf6e4da
PedroMB22 <pedromb.bocco@gmail.com>
2023-06-15 20:37:06 -0300

    Standings by Season and Leagues id

--- api/src/controllers/standings/getStandingsBySeason.js
+++ api/src/controllers/standings/getStandingsBySeason.js
@@ -2,15 +2,16 @@ const standingService = require('../../services/standings/getStandings.service')
 
 exports.getStandingsBySeason = async (req, res) => {
     const { id } = req.params;
-  try {
-    const standing = await standingService.getStandingsBySeason(id); 
-    if (standing) {
-      res.status(200).json(standing);
-    } else {
-      res.status(404).json({ message: 'No Standings found' });
+    const { league_id } = req.query; 
+    try {
+        const standing = await standingService.getStandingsBySeasonAndLeague(id, league_id); 
+        if (standing) {
+            res.status(200).json(standing);
+        } else {
+            res.status(404).json({ message: 'No Standings found' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Server error" });
     }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Server error" });
-  }
 };
--- api/src/services/standings/getStandings.service.js
+++ api/src/services/standings/getStandings.service.js
@@ -20,19 +20,20 @@ standingService.getStandings= async () => {
   }
 };
 
-standingService.getStandingsBySeason = async (id) => {
-    const URL = `${BASE_URL}/standings/seasons/${id}`;
-    try {
-        const response = await axios.get(URL, {
-            params: {
-                api_token: API_KEY
-            }
-        });
-        return response.data.data;
-    } catch (error) {
-        console.error(`Error fetching data from API: ${error}`);
-        throw error;
-    }
+standingService.getStandingsBySeasonAndLeague = async (seasonId, leagueId = null) => {
+  const URL = `${BASE_URL}/standings/seasons/${seasonId}`;
+  try {
+      const params = { api_token: API_KEY };
+      if (leagueId !== null) {
+          params.league_id = leagueId;
+      }
+      const response = await axios.get(URL, { params });
+      return response.data.data;
+  } catch (error) {
+      console.error(`Error fetching data from API: ${error}`);
+      throw error;
+  }
 };
+
 
 module.exports = standingService;
