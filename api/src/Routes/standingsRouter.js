05a078ae67bc4c53f10ef8f7c8718260d694bfaf
PedroMB22 <pedromb.bocco@gmail.com>
2023-06-16 14:45:37 -0300

    merge

--- api/src/Routes/standingsRouter.js
+++ api/src/Routes/standingsRouter.js
@@ -1,17 +1,10 @@
 const express = require("express");
 const standingsRouter = express.Router();
 
-<<<<<<< HEAD
-
-const { getStandingsBySeason } = require("../controllers/standings/getStandingsBySeason");
-
-
-=======
 //const { getStandings } = require("../controllers/standings/getStandings");
 const { getStandingsBySeason } = require("../controllers/standings/getStandingsBySeason");
 
 //standingsRouter.get("/", getStandings);
->>>>>>> controllers
 standingsRouter.get("/:id", getStandingsBySeason);
 
 module.exports = standingsRouter;
