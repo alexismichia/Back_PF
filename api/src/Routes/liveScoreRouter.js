2644a207d1d20f974f457d2a2388b0449fd9201d
oceanamassano <oshimassano@gmail.com>
2023-06-20 13:50:14 -0300

    testing

---
+++ api/src/Routes/liveScoreRouter.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const liveScoreRouter = express.Router();
+
+const {getLiveScore} = require("../controllers/livescores/getLiveScore")
+
+liveScoreRouter.get("/", getLiveScore)
+
+module.exports = liveScoreRouter;
---
+++ api/src/controllers/livescores/getLiveScore.js
@@ -0,0 +1,15 @@
+const {getLiveScoreFromAPI} = require("../../services/livescores/getLiveScore")
+
+exports.getLiveScore = async (req, res) => {
+  try {
+    const livescores = await getLiveScoreFromAPI(id);
+    if (livescores) {
+      res.status(200).json(livescores);
+    } else {
+      res.status(404).json({ message: "LiveScores not found" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
