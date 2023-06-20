edc26f134977313340f8049ed7cca4619868ebef
oceanamassano <oshimassano@gmail.com>
2023-06-20 14:03:31 -0300

    livescore inplay

--- api/src/Routes/liveScoreRouter.js
+++ api/src/Routes/liveScoreRouter.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const liveScoreRouter = express.Router();
 
-const {getLiveScore} = require("../controllers/livescores/getLiveScore")
+const { getLiveScore } = require("../controllers/livescores/getLiveScore");
 
-liveScoreRouter.get("/", getLiveScore)
+liveScoreRouter.get("/", getLiveScore);
 
 module.exports = liveScoreRouter;
--- api/src/controllers/livescores/getLiveScore.js
+++ api/src/controllers/livescores/getLiveScore.js
@@ -1,12 +1,22 @@
 const {getLiveScoreFromAPI} = require("../../services/livescores/getLiveScore")
-
+const {getLiveScoreInplayFromAPI} = require("../../services/livescores/getLiveScoreInplay")
 exports.getLiveScore = async (req, res) => {
+    const {inplay} = req.query
   try {
-    const livescores = await getLiveScoreFromAPI();
-    if (livescores) {
-      res.status(200).json(livescores);
-    } else {
-      res.status(404).json({ message: "LiveScores not found" });
+    if(inplay){
+        const livescores = await getLiveScoreInplayFromAPI();
+        if (livescores) {
+          res.status(200).json(livescores);
+        } else {
+          res.status(404).json({ message: "LiveScores not found" });
+        }
+    }else{
+        const livescores = await getLiveScoreFromAPI();
+        if (livescores) {
+          res.status(200).json(livescores);
+        } else {
+          res.status(404).json({ message: "LiveScores not found" });
+        }
     }
   } catch (err) {
     console.log(err);
