10dc877423ba2b0da9a6d3843ec805d42f2ec8d6
oceanamassano <oshimassano@gmail.com>
2023-06-21 22:23:25 -0300

    statistics by stage

--- api/src/Routes/statisticsRouter.js
+++ api/src/Routes/statisticsRouter.js
@@ -2,6 +2,9 @@ const express = require("express");
 const statisticsRouter = express.Router();
 
 const {getSeasonStatisticsByParticipant} = require("../controllers/statistics/getSeasonStatisticsByParticipant")
+const {getStageStatisticsById} = require("../controllers/statistics/getStageStatisticsById")
+//participants = coaches, players, teams or referees
 statisticsRouter.get("/:participant/:id", getSeasonStatisticsByParticipant)
+statisticsRouter.get("/stage/:id", getStageStatisticsById)
 
 module.exports = statisticsRouter;
---
+++ api/src/controllers/statistics/getStageStatisticsById.js
@@ -0,0 +1,14 @@
+exports.getStageStatisticsById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const statistics = await getStageStatisticsByIdFromAPI(id);
+    if (statistics) {
+      res.status(200).json(statistics);
+    } else {
+      res.status(404).json({ message: "Statistics not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
