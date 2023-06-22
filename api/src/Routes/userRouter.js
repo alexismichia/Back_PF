28cf9c93648b43b1c16e539642f4a4125c166241
Alexis Michia <alexi890@outlook.com>
2023-06-22 12:11:46 -0300

    user

--- api/src/Routes/userRouter.js
+++ api/src/Routes/userRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const userRouter = express.Router();
 const { authenticateJWT } = require("../middlewares/auth");
-const { createUser } = require("../controllers/user/postUser");
+
 const { updateUser } = require("../controllers/user/putUser");
-const {loginUser} = require("../controllers/user/loginUser")
+const {loginUser} = require("../controllers/user/loginUser");
+const { createUser } = require("../controllers/user/postUser");
 
 userRouter.post("/", createUser);
 userRouter.put("/:id", authenticateJWT, updateUser);
