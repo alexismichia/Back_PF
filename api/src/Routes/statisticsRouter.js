2ca18b93fcef1b976614059058bead161d75662b
oceanamassano <oshimassano@gmail.com>
2023-06-21 23:21:02 -0300

    stage change

--- api/src/Routes/statisticsRouter.js
+++ api/src/Routes/statisticsRouter.js
@@ -4,7 +4,7 @@ const statisticsRouter = express.Router();
 const {getSeasonStatisticsByParticipant} = require("../controllers/statistics/getSeasonStatisticsByParticipant")
 const {getStageStatisticsById} = require("../controllers/statistics/getStageStatisticsById")
 //participants = coaches, players, teams or referees
+statisticsRouter.get("stages/:id", getStageStatisticsById)
 statisticsRouter.get("/:participant/:id", getSeasonStatisticsByParticipant)
-statisticsRouter.get("/stages/:id", getStageStatisticsById)
 
 module.exports = statisticsRouter;
