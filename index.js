5f3980f6e4d4ed96bbae401833f5d79aa8a22c61
oceanamassano <oshimassano@gmail.com>
2023-06-23 11:27:42 -0300

    getLivesScoresByState fully functional

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ const { conn } = require('./api/src/db.js');
 
 
 conn.sync({ force: true }).then(() => {
-  const port = process.env.PORT || 3000;
-  server.listen(port, "0.0.0.0", () => {
-    console.log(`Server listening at 0.0.0.0:${port}`);
+  server.listen(3001, () => {
+    console.log(`Server listening at 3001`);
 
   });
 });
