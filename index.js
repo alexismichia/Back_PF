57834844a9be661a5144d6c1a5baaf222bae61a9
oceanamassano <oshimassano@gmail.com>
2023-06-23 11:41:44 -0300

    index updated

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ require('dotenv').config();
 const server = require('./api/src/app.js');
 const { conn } = require('./api/src/db.js');
 
+
+
 conn.sync({ force: true }).then(() => {
   const port = process.env.PORT || 3000;
-  server.listen(port, "3001", () => {
+  server.listen(port, "0.0.0.0", () => {
     console.log(`Server listening at 0.0.0.0:${port}`);
 
   });
 });
+
+// conn.sync({force:true}).then(()=>{
+//   server.listen(3001, ()=>{
+//     console.log("Server listening at port 3001")
+//   })
+// })
