116962fe04b9326dcea099370d8ffbf9877afcc5
Alexis Michia <alexi890@outlook.com>
2023-06-23 17:39:27 -0300

    prueba 2.0

--- api/src/controllers/team/getTeamByCountry.js
+++ api/src/controllers/team/getTeamByCountry.js
@@ -1,12 +1,12 @@
 const teamService = require('../../services/team/getTeam.service');
-const {TeamModel} = require('../../db')
+const Team = require('../../models/Team');
 
 exports.getTeam = async (req, res) => {
   const { id } = req.params;
   try {
     const team = await teamService.getTeamsByCountry(id); 
     if (team) {
-      const newteam= await TeamModel.create(team)
+      const newteam= await Team.create(team)
       res.status(200).json(team);
     } else {
       res.status(404).json({ message: 'No team found' });
