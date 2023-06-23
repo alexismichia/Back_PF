65d0edf78b4da458ceb9885900133008be3edfa4
oceanamassano <oshimassano@gmail.com>
2023-06-22 21:04:07 -0300

    included app password

--- api/src/email.js
+++ api/src/email.js
@@ -5,26 +5,26 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "footballcentraladm@gmail.com",
-    pass: "PROYECTOfinal2023",
+    user: GMAIL_USER,
+    pass: GMAIL_PASSWORD,
   },
 });
 
 const emailNewUser = (email) => {
-    const mailOptions ={
-        from: "footballcentraladm@gmail.com",
-        to: email,
-        subject: "Football Central account",
-        text:"Congratulations! Your account was created successfully"
+  const mailOptions = {
+    from: GMAIL_USER,
+    to: email,
+    subject: "Football Central account",
+    text: "Congratulations! Your account was created successfully",
+  };
+  
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(`Error sending email: ${error}`);
+    } else {
+      console.log(`Email sent succesfully: ${info.response}`);
     }
-    transporter.sendMail(mailOptions,(error, info)=>{
-        if(error){
-            console.log(`Error sending email: ${error}`)
-        }
-        else{
-            console.log(`Email sent succesfully: ${info.response}`)
-        }
-    })
+  });
 };
 
-module.exports = {emailNewUser}
+module.exports = { emailNewUser };
