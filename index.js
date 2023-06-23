80ecc6c98594c25b30a63dda370862376d2f6016
oceanamassano <oshimassano@gmail.com>
2023-06-22 21:10:40 -0300

    alter true

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const server = require('./api/src/app.js');
 const { conn } = require('./api/src/db.js');
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true}).then(() => {
   server.listen(3001, () => {
     console.log('%s listening at 3001'); 
   });
