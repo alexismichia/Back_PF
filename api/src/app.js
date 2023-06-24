22724202908b0eab56dd8f4e536c3c7447f08b24
PedroMB22 <pedromb.bocco@gmail.com>
2023-06-24 18:40:16 -0300

    cors ready

--- api/src/app.js
+++ api/src/app.js
@@ -14,7 +14,7 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use(cors())
+server.use(cors())  // cors ready
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); 
   res.header('Access-Control-Allow-Credentials', 'true');
