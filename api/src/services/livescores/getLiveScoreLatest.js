0c8d3d1b641a292cd3879dfc2521bbfe28d5c86d
oceanamassano <oshimassano@gmail.com>
2023-06-24 20:44:28 -0300

    trying includes

---
+++ api/src/services/livescores/getLiveScoreLatest.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+require("dotenv").config();
+
+const API_KEY = process.env.API_KEY;
+const BASE_URL = "https://api.sportmonks.com/v3/football/livescores";
+const league = "league"
+exports.getLiveScoreLatestFromAPI = async () => {
+  try {
+    const response = await axios.get(BASE_URL,{
+      params:{
+        api_token: API_KEY,
+        include: league
+      }
+    });
+    return response.data.data;
+  } catch (error) {
+    console.error(`Error fetching data from API: ${error}`);
+    throw error;
+  }
+};
