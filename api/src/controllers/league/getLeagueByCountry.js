d377ccd0f240bfd4da80d50c4d03cb134dc418bd
oceanamassano <oshimassano@gmail.com>
2023-06-26 11:34:40 -0300

    changed

--- api/src/controllers/league/getLeagueByCountry.js
+++ api/src/controllers/league/getLeagueByCountry.js
@@ -22,28 +22,10 @@ exports.getLeagueByCountry = async (req, res) => {
           has_jerseys: item.has_jerseys,
           
         }));
-        const newLeagues = await League.findOrCreate( {where: { id: newLeagueData.id },
+        const newLeagues = await League.findOrCreate( {where: { id: id },
           defaults: newLeagueData,});
-        res.status(200).json(newLeagues);
-      } else if (league.length === 1) {
-        // Si hay un solo nombre
-        const newLeagueData = {
-          id: league.id,
-          name: league.name,
-          active: league.active,
-          short_code: league.short_code,
-          image_path: league.image_path,
-          type: league.type,
-          sub_type: league.sub_type,
-          last_played_at: league.last_played_at,
-          category: league.category,
-          has_jerseys: league.has_jerseys,
-          
-        };
-        
-        const newLeagues = await League.create(newLeagueData);
-        res.status(200).json(newLeagues);
-    } else {
+        res.status(200).json(newLeagues);}
+ else {
       res.status(404).json({ message: "League not found" });
     }
   }} catch (error) {
