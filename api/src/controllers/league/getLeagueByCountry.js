86f5c15ef27977983086c5d61ef41b67128631b4
oceanamassano <oshimassano@gmail.com>
2023-06-26 11:41:03 -0300

    getLeagueBycountry not working

--- api/src/controllers/league/getLeagueByCountry.js
+++ api/src/controllers/league/getLeagueByCountry.js
@@ -1,30 +1,32 @@
-const { getLeagueCountryFromAPI } = require("../../services/league/getLeagueByCountry.service");
-const {League}= require("../../db")
-
+const {
+  getLeagueCountryFromAPI,
+} = require("../../services/league/getLeagueByCountry.service");
+const { League } = require("../../db");
 
 exports.getLeagueByCountry = async (req, res) => {
   const { id } = req.params;
   try {
     const league = await getLeagueCountryFromAPI(id);
     if (league) {
-        // Si hay varios nombres
-        const newLeagueData = {
-          id: league.id,
-          name: league.name,
-          active: league.active,
-          short_code: league.short_code,
-          image_path: league.image_path,
-          type: league.type,
-          sub_type: league.sub_type,
-          last_played_at: league.last_played_at,
-          category: league.category,
-          has_jerseys: league.has_jerseys,
-          
-        };
-        const newLeagues = await League.findOrCreate( {where: { id: newLeagueData.id },
-          defaults: newLeagueData,});
-        res.status(200).json(newLeagues);}
- else {
+      const newLeagueData = {
+        id: league.id,
+        name: league.name,
+        active: league.active,
+        short_code: league.short_code,
+        image_path: league.image_path,
+        type: league.type,
+        sub_type: league.sub_type,
+        last_played_at: league.last_played_at,
+        category: league.category,
+        has_jerseys: league.has_jerseys,
+      };
+      console.log(newLeagueData)
+      const newLeagues = await League.findOrCreate({
+        where: { id: newLeagueData.id },
+        defaults: newLeagueData,
+      });
+      res.status(200).json(newLeagues);
+    } else {
       res.status(404).json({ message: "League not found" });
     }
   } catch (error) {
