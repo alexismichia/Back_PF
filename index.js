4e47560924cd7e8440779b94019302e81a2822d6
Alexis Michia <alexi890@outlook.com>
2023-06-27 20:25:05 -0300

    index

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ require('dotenv').config();
 const server = require('./api/src/app.js');
 const { conn } = require('./api/src/db.js');
 
- conn.sync({ force: false }).then(() => {
+ /* conn.sync({ force: false }).then(() => {
    server.listen(3001, () => {
      console.log('%s listening at 3001'); 
    });
- });
+ }); */
 
-/* conn.sync({ force: true }).then(() => {
+conn.sync({ force: true }).then(() => {
   const port = process.env.PORT || 3000;
   server.listen(port, "0.0.0.0", () => {
     console.log(`Server listening at 0.0.0.0:${port}`);
 
   });
-}); */
+});
 
 // conn.sync({ force: true }).then(() => {
 //   const port = process.env.PORT || 3000;
