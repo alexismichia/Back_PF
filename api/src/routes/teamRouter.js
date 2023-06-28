5f7ce0ef35df6f71e92948ad87bcda405d5e520d
Alexis Michia <alexi890@outlook.com>
2023-06-28 10:51:26 -0300

    arreglado

--- api/src/routes/teamRouter.js
+++ api/src/routes/teamRouter.js
@@ -6,7 +6,8 @@ const {getTeam} = require('../controllers/team/getTeamByCountry.js')
 const {getTeamById} = require('../controllers/team/getTeamById.js')    
 const {getTeamBySeason} = require('../controllers/team/getTeamBySeason.js')
 const {getTeamByName} = require('../controllers/team/getTeamByName.js')
-const { getTeams } = require('../controllers/team/getteams.js')
+const { getTeams } = require('../controllers/team/getTeams.js')
+
 
 
 teamRouter.get('/country/:id', getTeam)
