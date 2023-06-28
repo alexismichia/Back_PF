654f0a5a39e3bcdaeb6300b8329756ea539acc93
Alexis Michia <alexi890@outlook.com>
2023-06-28 11:01:58 -0300

    eliminacion masiva

--- api/src/routes/commentaryRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const commentaryRouter = express.Router();
-
-const {getCommentary} = require("../controllers/commentary/getCommentary")
-
-//id is fixture id
-commentaryRouter.get("/:id", getCommentary)
-
-module.exports = commentaryRouter;
--- api/src/routes/standingsRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const standingsRouter = express.Router();
-//const { getStandings } = require("../controllers/standings/getStandings");
-const { getStandingsBySeason } = require("../controllers/standings/getStandingsBySeason");
-
-//standingsRouter.get("/", getStandings);
-standingsRouter.get("/:id", getStandingsBySeason);
-
-module.exports = standingsRouter;
