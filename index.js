4126a2733d47bba995b79943081bcce0a7fa4ed5
oceanamassano <oshimassano@gmail.com>
2023-06-29 09:58:51 -0300

    force true

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { conn } = require('./api/src/db.js');
 //   });
 // });
 
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: true }).then(() => {
   const port = process.env.PORT || 3000;
   server.listen(port, "0.0.0.0", () => {
     console.log(`Server listening at 0.0.0.0:${port}`);
