edf659abe3cf46880c22c82c161237a9a28442d8
oceanamassano <oshimassano@gmail.com>
2023-06-30 12:42:00 -0300

    changed db and index

--- api/src/db.js
+++ api/src/db.js
@@ -31,23 +31,23 @@ const {
   ReviewsModel,
 } = require("./models/index");
 
-  const sequelize = new Sequelize(
-     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/postgres`,
-     {
-       logging: false,
-       native: false,
-     }
-   ); 
+  // const sequelize = new Sequelize(
+  //    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/postgres`,
+  //    {
+  //      logging: false,
+  //      native: false,
+  //    }
+  //  ); 
 
  const { DATABASE_URL, PGDATABASE, PGHOST, PGPASSWORD, PGPORT, PGUSER } = process.env;
 
-// const sequelize = new Sequelize(
-//   `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`,
-//   {
-//     logging: false,
-//     native: false,
-//   }
-// );  
+const sequelize = new Sequelize(
+  `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`,
+  {
+    logging: false,
+    native: false,
+  }
+);  
 //connects models to sequelize
 CoachModel(sequelize);
 FixtureModel(sequelize);
