a39c405f71a4d2910a7b7bc45f57cdc3497f608b
PedroMB22 <pedromb.bocco@gmail.com>
2023-06-30 19:00:47 -0300

    getUsersById

--- api/src/controllers/user/getUserById.js
+++ api/src/controllers/user/getUserById.js
@@ -1,19 +1,20 @@
-// const userService = require('../../services/user/user.service');
+const userService = require('../../services/user/user.service');
 
-// const getUserId = async (req, res) => {
-//   const { id } = req.params;
+const getUserId = async (req, res) => {
+  const { id } = req.params;
+  console.log("id", id);
 
-//   try {
-//     const user = await userService.getUser(id);
-//     if (user) {
-//       res.status(200).json(user);
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     res.status(400).json({ message: error.message });
-//   }
-// };
+  try {
+    const user = await userService.getUserById(id);
+    if (user) {
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
+};
 
-// module.exports = {
-//   getUserId,
-// };
+module.exports = {
+  getUserId,
+};
