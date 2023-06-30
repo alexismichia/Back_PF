5523d70488654fcbc3c7c92270321cc2b668e366
oceanamassano <oshimassano@gmail.com>
2023-06-30 10:08:17 -0300

    fixed playerServices

--- api/src/services/Player/getPlayers.service.js
+++ api/src/services/Player/getPlayers.service.js
@@ -5,14 +5,27 @@ const API_KEY = process.env.API_KEY;
 const BASE_URL = "https://api.sportmonks.com/v3/football/players";
 
 exports.PlayerService = async (name) => {
-const URL = `${BASE_URL}/search/${name}`
+  let URL = `${BASE_URL}/${name}`;
+  const allData = [];
   try {
-    const response = await axios.get(URL,{
-      params:{
-        api_token: API_KEY
+    while (URL) {
+      const response = await axios.get(URL, {
+        params: {
+          // per_page: 2,  for tests
+          api_token: API_KEY,
+        },
+      });
+      const responseData = response.data.data;
+      allData.push(...responseData);
+
+      const pagination = response.data.pagination;
+      URL = pagination.next_page;
+
+      if (!pagination.has_more) {
+        URL = null;
       }
-    });
-    return response.data.data;
+    }
+    return allData;
   } catch (error) {
     console.error(`Error fetching data from API: ${error}`);
     throw error;
