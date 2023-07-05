bf25a4569c450c82c46e60b384b49e1b3fcd65de
mateomohr02 <mohr.mateo@gmail.com>
2023-07-05 19:51:38 -0300

    changes deploy

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const { conn } = require('./api/src/db.js');
 
 
 
- conn.sync({ alter: true }).then(() => {
-   server.listen(3001, () => {
-     console.log('%s listening at 3001'); 
+//  conn.sync({ alter: true }).then(() => {
+//    server.listen(3001, () => {
+//      console.log('%s listening at 3001'); 
+//    });
+//  }); 
+
+
+  conn.sync({ force: false }).then(() => {
+   const port = process.env.PORT || 3001;
+   server.listen(port, "0.0.0.0", () => {
+     console.log(`Server listening at 0.0.0.0:${port}`);
+
    });
  }); 
-
-
-//  conn.sync({ force: false }).then(() => {
-//   const port = process.env.PORT || 3000;
-//   server.listen(port, "0.0.0.0", () => {
-//     console.log(`Server listening at 0.0.0.0:${port}`);
-
-//   });
-// }); 
 
 
